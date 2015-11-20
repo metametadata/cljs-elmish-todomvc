34ccc6ffe6ba8521771c305ba6beed71cccca066
metametadata <yuri.govor@gmail.com>
2015-11-20 21:25:34 +0200

    Update documentation

--- js/compiled/out/figwheel/connect.js
+++ js/compiled/out/figwheel/connect.js
@@ -5,29 +5,29 @@ goog.require('frontend.core');
 goog.require('figwheel.client');
 goog.require('figwheel.client.utils');
 figwheel.client.start.call(null,new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"on-jsload","on-jsload",-395756602),(function() { 
-var G__27460__delegate = function (x){
+var G__21435__delegate = function (x){
 if(cljs.core.truth_(frontend.core.on_js_reload)){
 return cljs.core.apply.call(null,frontend.core.on_js_reload,x);
 } else {
 return figwheel.client.utils.log.call(null,new cljs.core.Keyword(null,"debug","debug",-1608172596),"Figwheel: :on-jsload hook 'frontend.core/on-js-reload' is missing");
 }
 };
-var G__27460 = function (var_args){
+var G__21435 = function (var_args){
 var x = null;
 if (arguments.length > 0) {
-var G__27461__i = 0, G__27461__a = new Array(arguments.length -  0);
-while (G__27461__i < G__27461__a.length) {G__27461__a[G__27461__i] = arguments[G__27461__i + 0]; ++G__27461__i;}
-  x = new cljs.core.IndexedSeq(G__27461__a,0);
+var G__21436__i = 0, G__21436__a = new Array(arguments.length -  0);
+while (G__21436__i < G__21436__a.length) {G__21436__a[G__21436__i] = arguments[G__21436__i + 0]; ++G__21436__i;}
+  x = new cljs.core.IndexedSeq(G__21436__a,0);
 } 
-return G__27460__delegate.call(this,x);};
-G__27460.cljs$lang$maxFixedArity = 0;
-G__27460.cljs$lang$applyTo = (function (arglist__27462){
-var x = cljs.core.seq(arglist__27462);
-return G__27460__delegate(x);
+return G__21435__delegate.call(this,x);};
+G__21435.cljs$lang$maxFixedArity = 0;
+G__21435.cljs$lang$applyTo = (function (arglist__21437){
+var x = cljs.core.seq(arglist__21437);
+return G__21435__delegate(x);
 });
-G__27460.cljs$core$IFn$_invoke$arity$variadic = G__27460__delegate;
-return G__27460;
+G__21435.cljs$core$IFn$_invoke$arity$variadic = G__21435__delegate;
+return G__21435;
 })()
 ,new cljs.core.Keyword(null,"build-id","build-id",1642831089),"dev",new cljs.core.Keyword(null,"websocket-url","websocket-url",-490444938),"ws://localhost:3449/figwheel-ws"], null));
 
-//# sourceMappingURL=connect.js.map?rel=1448046309485
+//# sourceMappingURL=connect.js.map?rel=1448046419488
