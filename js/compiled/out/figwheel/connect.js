1b9d26739502c4b62cac2fa5f542e1463779991d
metametadata <yuri.govor@gmail.com>
2015-11-23 03:45:44 +0200

    Update documentation

--- js/compiled/out/figwheel/connect.js
+++ js/compiled/out/figwheel/connect.js
@@ -5,29 +5,29 @@ goog.require('frontend.core');
 goog.require('figwheel.client');
 goog.require('figwheel.client.utils');
 figwheel.client.start.call(null,new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"on-jsload","on-jsload",-395756602),(function() { 
-var G__44366__delegate = function (x){
+var G__44935__delegate = function (x){
 if(cljs.core.truth_(frontend.core.on_js_reload)){
 return cljs.core.apply.call(null,frontend.core.on_js_reload,x);
 } else {
 return figwheel.client.utils.log.call(null,new cljs.core.Keyword(null,"debug","debug",-1608172596),"Figwheel: :on-jsload hook 'frontend.core/on-js-reload' is missing");
 }
 };
-var G__44366 = function (var_args){
+var G__44935 = function (var_args){
 var x = null;
 if (arguments.length > 0) {
-var G__44367__i = 0, G__44367__a = new Array(arguments.length -  0);
-while (G__44367__i < G__44367__a.length) {G__44367__a[G__44367__i] = arguments[G__44367__i + 0]; ++G__44367__i;}
-  x = new cljs.core.IndexedSeq(G__44367__a,0);
+var G__44936__i = 0, G__44936__a = new Array(arguments.length -  0);
+while (G__44936__i < G__44936__a.length) {G__44936__a[G__44936__i] = arguments[G__44936__i + 0]; ++G__44936__i;}
+  x = new cljs.core.IndexedSeq(G__44936__a,0);
 } 
-return G__44366__delegate.call(this,x);};
-G__44366.cljs$lang$maxFixedArity = 0;
-G__44366.cljs$lang$applyTo = (function (arglist__44368){
-var x = cljs.core.seq(arglist__44368);
-return G__44366__delegate(x);
+return G__44935__delegate.call(this,x);};
+G__44935.cljs$lang$maxFixedArity = 0;
+G__44935.cljs$lang$applyTo = (function (arglist__44937){
+var x = cljs.core.seq(arglist__44937);
+return G__44935__delegate(x);
 });
-G__44366.cljs$core$IFn$_invoke$arity$variadic = G__44366__delegate;
-return G__44366;
+G__44935.cljs$core$IFn$_invoke$arity$variadic = G__44935__delegate;
+return G__44935;
 })()
 ,new cljs.core.Keyword(null,"build-id","build-id",1642831089),"dev",new cljs.core.Keyword(null,"websocket-url","websocket-url",-490444938),"ws://localhost:3449/figwheel-ws"], null));
 
-//# sourceMappingURL=connect.js.map?rel=1448242671486
+//# sourceMappingURL=connect.js.map?rel=1448243075602
--- js/compiled/out/frontend/ui.js
+++ js/compiled/out/frontend/ui.js
@@ -4,7 +4,7 @@ goog.require('cljs.core');
 goog.require('reagent.core');
 goog.require('cljs.pprint');
 /**
- * Model must be immutable. First signal can be nil if it should not be fired.
+ * Initial model must be immutable. Initial signal can be nil if it should not be fired.
  *   Control can be a non-pure function.
  *   View-model, view and reconcile must be pure functions.
  * 
@@ -19,33 +19,33 @@ goog.require('cljs.pprint');
  *   Data flow:
  *   model -> (view-model) -> (view) -signal-> (control) -action-> (reconcile) -> model -> etc.
  */
-frontend.ui.connect = (function frontend$ui$connect(p__43780,view_model,view,control,reconcile){
-var vec__43783 = p__43780;
-var model = cljs.core.nth.call(null,vec__43783,(0),null);
-var first_signal = cljs.core.nth.call(null,vec__43783,(1),null);
-var _init_ = vec__43783;
+frontend.ui.connect = (function frontend$ui$connect(p__44371,view_model,view,control,reconcile){
+var vec__44374 = p__44371;
+var model = cljs.core.nth.call(null,vec__44374,(0),null);
+var signal = cljs.core.nth.call(null,vec__44374,(1),null);
+var _initial_ = vec__44374;
 var model_atom = reagent.core.atom.call(null,model);
-var dispatch_action = ((function (model_atom,vec__43783,model,first_signal,_init_){
+var dispatch_action = ((function (model_atom,vec__44374,model,signal,_initial_){
 return (function (a){
 cljs.core.swap_BANG_.call(null,model_atom,reconcile,a);
 
 return null;
-});})(model_atom,vec__43783,model,first_signal,_init_))
+});})(model_atom,vec__44374,model,signal,_initial_))
 ;
-var dispatch_signal = ((function (model_atom,dispatch_action,vec__43783,model,first_signal,_init_){
+var dispatch_signal = ((function (model_atom,dispatch_action,vec__44374,model,signal,_initial_){
 return (function (s){
 control.call(null,cljs.core.deref.call(null,model_atom),s,dispatch_action);
 
 return null;
-});})(model_atom,dispatch_action,vec__43783,model,first_signal,_init_))
+});})(model_atom,dispatch_action,vec__44374,model,signal,_initial_))
 ;
-var connected_view = ((function (model_atom,dispatch_action,dispatch_signal,vec__43783,model,first_signal,_init_){
+var connected_view = ((function (model_atom,dispatch_action,dispatch_signal,vec__44374,model,signal,_initial_){
 return (function (){
 return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [view,view_model.call(null,cljs.core.deref.call(null,model_atom)),dispatch_signal], null);
-});})(model_atom,dispatch_action,dispatch_signal,vec__43783,model,first_signal,_init_))
+});})(model_atom,dispatch_action,dispatch_signal,vec__44374,model,signal,_initial_))
 ;
-var G__43784_43785 = first_signal;
-var G__43784_43786__$1 = (((G__43784_43785 == null))?null:dispatch_signal.call(null,G__43784_43785));
+var G__44375_44376 = signal;
+var G__44375_44377__$1 = (((G__44375_44376 == null))?null:dispatch_signal.call(null,G__44375_44376));
 
 return new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"view","view",1247994814),connected_view,new cljs.core.Keyword(null,"dispatch-signal","dispatch-signal",205544591),dispatch_signal,new cljs.core.Keyword(null,"model","model",331153215),model_atom,new cljs.core.Keyword(null,"dispatch-action","dispatch-action",-1863756601),dispatch_action], null);
 });
@@ -74,4 +74,4 @@ return result;
 });
 });
 
-//# sourceMappingURL=ui.js.map?rel=1448242535494
+//# sourceMappingURL=ui.js.map?rel=1448242902943
